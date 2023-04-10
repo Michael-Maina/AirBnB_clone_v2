7426e0b8f25c53362ec3237a78050c627cd7a723
Michael-Maina <michaelmaina02@gmail.com>
2023-04-10 04:45:38 +0300

    Bug fixing

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,35 +1,30 @@
 #!/usr/bin/python3
-"""Do deploy web static module"""
-from datetime import datetime
-from os import path
-from fabric import api
-
-api.env.user = 'ubuntu'
-api.env.hosts = ['100.26.172.132', '54.209.159.164']
+"""
+script that distributes archive to webservers
+"""
+import os.path
+from fabric.api import *
+from fabric.operations import run, put, sudo
+env.hosts = ['100.26.172.132', '54.209.159.164']
 
 
 def do_deploy(archive_path):
-    """
-    Fabric script (based on the file 1-pack_web_static.py) that distributes
-    an archive to your web servers, using the function do_deploy
-    """
-    if not path.exists(archive_path):
+    """ deploy """
+    if (os.path.isfile(archive_path) is False):
         return False
+
     try:
-        file_name = archive_path.split("/")[-1]
-        just_name = file_name.split(".")[0]
-        api.put(archive_path, "/tmp/")
-        api.run("mkdir -p /data/web_static/releases/{}".format(just_name))
-        api.run("tar -xzf /tmp/{} -C /data/web_static/releases/{}".format(
-                                                        file_name, just_name))
-        api.run("rm /tmp/{}".format(file_name))
-        path_r = "/data/web_static/releases/"
-        api.run('mv {0}{1}/web_static/* {0}{1}/'.format(path_r, just_name))
-        api.run('rm -rf /data/web_static/releases/{}/web_static'.format(
-                                                                just_name))
-        api.run("rm -rf /data/web_static/current")
-        api.run("ln -s /data/web_static/releases/{} \
-                    /data/web_static/current".format(just_name))
+        new_comp = archive_path.split("/")[-1]
+        new_folder = ("/data/web_static/releases/" + new_comp.split(".")[0])
+        put(archive_path, "/tmp/")
+        run("sudo mkdir -p {}".format(new_folder))
+        run("sudo tar -xzf /tmp/{} -C {}".
+            format(new_comp, new_folder))
+        run("sudo rm /tmp/{}".format(new_comp))
+        run("sudo mv {}/web_static/* {}/".format(new_folder, new_folder))
+        run("sudo rm -rf {}/web_static".format(new_folder))
+        run('sudo rm -rf /data/web_static/current')
+        run("sudo ln -s {} /data/web_static/current".format(new_folder))
         return True
     except:
         return False
