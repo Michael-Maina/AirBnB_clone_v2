13b1a2f34b23a62671dbc775e9003dc1e90b9f69
Michael-Maina <michaelmaina02@gmail.com>
2023-04-23 11:08:43 +0300

    Added task 0

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Script that starts a Flask web application:
+
+Web application must be listening on 0.0.0.0, port 5000
+Routes:
+/: display “Hello HBNB!”
+"""
+from flask import Flask
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route("/")
+def hello_hbnb():
+    return "Hello HBNB!"
