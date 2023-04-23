e43b29d1b1cbc697745b2aab14192c5f87c6a3ee
Michael-Maina <michaelmaina02@gmail.com>
2023-04-23 16:42:32 +0300

    Style fix

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -41,7 +41,7 @@ class FileStorage:
         from models.amenity import Amenity
         from models.review import Review
 
-        classes = {
+        clses = {
                     'BaseModel': BaseModel, 'User': User, 'Place': Place,
                     'State': State, 'City': City, 'Amenity': Amenity,
                     'Review': Review
@@ -51,7 +51,7 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                    FileStorage.__objects[key] = classes[val['__class__']](**val)
+                    FileStorage.__objects[key] = clses[val['__class__']](**val)
         except FileNotFoundError:
             pass
 
